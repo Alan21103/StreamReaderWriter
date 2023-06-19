24433306a4a789bda230942955fb3d884faac2fd
Alan21103 <syahlansaputra03@gmail.com>
2023-06-19 11:48:51 +0700

    Menampilkan perintah masukkan nama file dan membuka file dalam mode menulis

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -6,3 +6,14 @@ using namespace std;
 int main() {
 	string baris;
 	string NamaFile;
+
+	cout << "Masukan Nama File : ";
+	cin >> NamaFile;
+
+	//membuka file dalam mode menulis
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open(NamaFile, ios::out);
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
